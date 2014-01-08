c39c38c812caa78faeb6b2d3d5b4a43f440ab394
sergiokv13 <sergiokv13@gmail.com>
2014-01-08 05:44:12 -0400

    primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puka::Application.config.secret_key_base = 'babb47fac185d950799c894b5214cffe9a6787afbcef1ec4a645eca96bb8f44ab8d1c46d1736d29309ff962820f6b56749bd920ab7b696ea0faf7f1c83280a69'
